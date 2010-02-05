cf39f54637831fd160be25824c4b387e7d9be9a9
Alistair Riddoch <alriddoch@googlemail.com>
2010-02-05 19:28:09 +0000

    Try out libxml2.

--- libcolladadae/tests/dumpdae.c
+++ libcolladadae/tests/dumpdae.c
@@ -1,4 +1,10 @@
-int main()
+#include <libxml/parser.h>
+
+int main(int argc, char ** argv)
 {
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
     return 0;
 }
