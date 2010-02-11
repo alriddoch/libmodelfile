942c13c30a999870467fc10fa8fc70915b486310
Alistair Riddoch <alriddoch@googlemail.com>
2010-02-11 02:54:32 +0000

    Add the library sources to the build.

--- libcolladadae/tests/dumpdae.c
+++ libcolladadae/tests/dumpdae.c
@@ -1,9 +1,18 @@
-#include <libxml/parser.h>
+#include <libcolladadae/parser.h>
+
+#include <stdio.h>
 
 int main(int argc, char ** argv)
 {
+    libcolladadae_file * ret;
+
     if (argc != 2) {
         fprintf(stderr, "usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+    ret = libcolladadae_file_load(argv[1]);
+    if (ret == 0) {
+        fprintf(stderr, "Parsing %s failed\n", argv[1]);
         return 1;
     }
     return 0;
